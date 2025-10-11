ae0c314552208459dabe36f242c5fd4837f2f78a
Programming-Sai <saiahprog6@gmail.com>
2025-10-11 07:12:12 +0000

    feat(dashboard): complete dashboard screen and fix sidebar margin issue

---
+++ src/api/dashboard.js
@@ -0,0 +1,93 @@
+export const dashboard = [
+  {
+    title: "Total Users",
+    value: "12.6k",
+    change: "1.1%",
+    period: "6 months",
+  },
+  {
+    title: "Verified Users",
+    value: "12.6k",
+    change: "1.1%",
+    period: "6 months",
+  },
+  {
+    title: "User Growth",
+    stats: [
+      { label: "Today", value: "12.6k" },
+      { label: "Yesterday", value: "12.6k" },
+      { label: "7 days ago", value: "12.6k" },
+      { label: "1 month ago", value: "12.6k" },
+    ],
+    period: "6 months",
+  },
+  {
+    title: "Total Ads",
+    value: "12.6k",
+    change: "1.1%",
+    period: "6 months",
+  },
+  {
+    title: "Ads Active",
+    value: "12.6k",
+    change: "1.1%",
+    period: "6 months",
+  },
+  {
+    title: "Ads Pending",
+    value: "12.6k",
+    change: "1.1%",
+    period: "6 months",
+  },
+  {
+    title: "Ads Taken",
+    value: "12.6k",
+    change: "1.1%",
+    period: "6 months",
+  },
+  {
+    title: "Ads Suspended",
+    value: "12.6k",
+    change: "1.1%",
+    period: "6 months",
+  },
+  {
+    title: "Total Subscriptions",
+    value: "12.6k",
+    change: "1.1%",
+    period: "6 months",
+  },
+  {
+    title: "Total Premium",
+    value: "12.6k",
+    change: "1.1%",
+    period: "6 months",
+  },
+  {
+    title: "Active Standard",
+    value: "12.6k",
+    change: "1.1%",
+    period: "6 months",
+  },
+  {
+    title: "Active Basic",
+    value: "12.6k",
+    change: "1.1%",
+    period: "6 months",
+  },
+  {
+    title: "Total Revenue",
+    value: "12.6k",
+    change: "1.1%",
+    period: "6 months",
+  },
+  {
+    title: "Revenue Summary",
+    stats: [
+      { label: "Today Orders", value: "56" },
+      { label: "Yesterday", value: "45785" },
+      { label: "7 days ago", value: "578" },
+    ],
+    period: "6 months",
+  },
+];
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,40 @@
 import React from "react";
 import styles from "./dashboard.module.css";
+import { dashboard } from "../../api/dashboard";
+import { ArrowDown } from "../../components/SVGIcons/ArrowDown";
+import { ArrowUp } from "../../components/SVGIcons/ArrowUp";
 
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className={styles.dashboardContainer}>
+      {dashboard?.map((card, idx) => (
+        <div className={styles.gridBox} key={idx}>
+          <p className={styles.title}>{card?.title}</p>
+          {card?.stats && card?.stats?.length ? (
+            <div className={styles.gcard}>
+              {card.stats.map((stat, id) => (
+                <div key={id}>
+                  <p>{stat.label}</p>
+                  <h1>{stat.value}</h1>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div key={idx} className={styles.box}>
+              <div>
+                <h1>{card?.value}</h1>
+                <p>{card?.period}</p>
+              </div>
+              <div
+                className={styles.change}
+                style={{ backgroundColor: idx % 2 ? "#F7CF94" : "#74FFA7" }}
+              >
+                {idx % 2 ? <ArrowDown /> : <ArrowUp />} {card?.change}
+              </div>
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
 };
