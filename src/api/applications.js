6d6df6a2f9c7b4f74f2077e0e3b54ad75f02f05a
Programming-Sai <saiahprog6@gmail.com>
2025-10-16 17:31:38 +0000

    Working on the Applications page. left with proper styling now

---
+++ src/api/applications.js
@@ -0,0 +1,62 @@
+export const applicationsData = [
+  {
+    img: "https://randomuser.me/api/portraits/men/12.jpg",
+    name: "Daniel Kery",
+    phone: "0552892433",
+    email: "agblod@gmail.com",
+    location: "Accra",
+    age: 18,
+    coverLetter:
+      "I am passionate about digital marketing and want to join Oysloe to learn and contribute to the team.",
+    dateApplied: "Today 12:00",
+    fileLink: "/files/cover-letter-daniel.pdf",
+  },
+  {
+    img: "https://randomuser.me/api/portraits/women/12.jpg",
+    name: "Ama Nyarko",
+    phone: "0246123489",
+    email: "ama.nyarko@example.com",
+    location: "Kumasi",
+    age: 22,
+    coverLetter:
+      "As a creative designer, I’d love to work with your visual team to bring modern brand stories to life.",
+    dateApplied: "Yesterday 4:35 PM",
+    fileLink: "/files/cover-letter-ama.pdf",
+  },
+  {
+    img: "https://randomuser.me/api/portraits/men/13.jpg",
+    name: "Michael Tetteh",
+    phone: "0592017456",
+    email: "miketetteh@gmail.com",
+    location: "Tema",
+    age: 25,
+    coverLetter:
+      "Experienced in logistics and coordination, I’m applying to assist with supply chain management.",
+    dateApplied: "2 days ago",
+    fileLink: "/files/cover-letter-mike.pdf",
+  },
+  {
+    img: "https://randomuser.me/api/portraits/women/1.jpg",
+    name: "Esi Boadu",
+    phone: "0509312245",
+    email: "esi.boadu@example.com",
+    location: "Takoradi",
+    age: 20,
+    coverLetter:
+      "As a student in marketing, I’m excited to gain practical experience through this internship.",
+    dateApplied: "3 days ago",
+    fileLink: "/files/cover-letter-esi.pdf",
+  },
+  {
+    img: "https://randomuser.me/api/portraits/men/50.jpg",
+    name: "Kwame Adjei",
+    phone: "0276110098",
+    email: "kwame.adjei@example.com",
+    location: "Cape Coast",
+    age: 24,
+    coverLetter:
+      "I’m applying for the technical assistant position to help maintain and improve digital operations.",
+    dateApplied: "Last week",
+    fileLink: "/files/cover-letter-kwame.pdf",
+  },
+];
--- src/pages/Applications/Applications.jsx
+++ src/pages/Applications/Applications.jsx
@@ -1,6 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./applications.module.css";
+import { SearchIcon } from "../../components/SVGIcons/SearchIcon";
+import { Caret } from "../../components/SVGIcons/Caret";
+import { applicationsData } from "../../api/applications";
+import ImageIcon from "../../components/SVGIcons/ImageIcon";
+import Download from "../../assets/downloadCopy.png";
 
 export const Applcations = () => {
-  return <div>Applciations</div>;
+  const [rowData, setRowData] = useState(null);
+  return (
+    <div className={styles.applicationsContainer}>
+      <div className={styles.header}>
+        <div className={styles.searchBox}>
+          <SearchIcon />
+          <input type="search" placeholder="Search" />
+        </div>
+        <div className={styles.dropDown}>
+          <p>All</p>
+          <Caret />
+        </div>
+      </div>
+      <p className={styles.total}>~ {"567"}</p>
+      <div className={styles.table}>
+        {applicationsData &&
+          applicationsData?.map((application, idx) => (
+            <div className={styles.row}>
+              <img src={application?.img} />
+              <p>{application?.name}</p>
+              <p>{application?.phone}</p>
+              <p>{application?.email}</p>
+              <p>{application?.location}</p>
+              <p>{application?.age}</p>
+              <button onClick={() => setRowData(application)}>
+                Cover Letter
+              </button>
+              <div className={styles.downloadAndTime}>
+                <button>
+                  <ImageIcon src={Download} />
+                  Download
+                </button>
+                <p>{application?.dateApplied}</p>
+              </div>
+            </div>
+          ))}
+      </div>
+      {rowData && (
+        <>
+          <div className={styles.backdrop} />
+          <div className={styles.modal}>
+            <h1>Cover Letter</h1>
+            <p> Dated {rowData?.dateApplied}</p>
+            <textarea value={rowData?.coverLetter} />
+            <button onClick={() => setRowData(null)}>Done</button>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
