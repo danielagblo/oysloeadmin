66a04604ccc3dc7ff643252692848c73e5b7d7e3
Programming-Sai <saiahprog6@gmail.com>
2025-10-17 18:18:41 +0000

    Working on the Locations Page now. finding issues with the sticky positioning. would work on that, styling and then dnd functionality.

---
+++ src/api/locations.js
@@ -0,0 +1,66 @@
+export const locationsData = [
+  {
+    region: "Greater Accra",
+    towns: ["Accra", "Tema", "Madina", "Teshie", "Nungua"],
+  },
+  {
+    region: "Ashanti",
+    towns: ["Kumasi", "Obuasi", "Ejisu", "Konongo", "Bekwai"],
+  },
+  {
+    region: "Western",
+    towns: ["Takoradi", "Sekondi", "Tarkwa", "Axim", "Bogoso"],
+  },
+  {
+    region: "Central",
+    towns: ["Cape Coast", "Winneba", "Elmina", "Mankessim", "Kasoa"],
+  },
+  {
+    region: "Eastern",
+    towns: ["Koforidua", "Nkawkaw", "Oda", "Akosombo", "Asamankese"],
+  },
+  {
+    region: "Volta",
+    towns: ["Ho", "Keta", "Aflao", "Kpando", "Akatsi"],
+  },
+  {
+    region: "Northern",
+    towns: ["Tamale", "Yendi", "Bimbilla", "Savelugu", "Karaga"],
+  },
+  {
+    region: "Upper East",
+    towns: ["Bolgatanga", "Navrongo", "Bawku", "Zuarungu", "Sandema"],
+  },
+  {
+    region: "Upper West",
+    towns: ["Wa", "Tumu", "Nandom", "Lawra", "Jirapa"],
+  },
+  {
+    region: "Bono",
+    towns: ["Sunyani", "Berekum", "Dormaa Ahenkro", "Wamfie"],
+  },
+  {
+    region: "Bono East",
+    towns: ["Techiman", "Atebubu", "Nkoranza", "Kintampo"],
+  },
+  {
+    region: "Ahafo",
+    towns: ["Goaso", "Bechem", "Hwidiem", "Duayaw Nkwanta"],
+  },
+  {
+    region: "Oti",
+    towns: ["Dambai", "Nkwanta", "Jasikan", "Kadjebi"],
+  },
+  {
+    region: "Savannah",
+    towns: ["Damongo", "Bole", "Salaga", "Larabanga"],
+  },
+  {
+    region: "North East",
+    towns: ["Nalerigu", "Walewale", "Gambaga", "Langbinsi"],
+  },
+  {
+    region: "Western North",
+    towns: ["Sefwi Wiawso", "Bibiani", "Juaboso", "Enchi"],
+  },
+];
--- src/pages/Locations/Locations.jsx
+++ src/pages/Locations/Locations.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 import styles from "./locations.module.css";
+import { locationsData } from "../../api/locations";
 
 export const Locations = () => {
-  return <div>Locations</div>;
+  return (
+    <div className={styles.locationsContainer}>
+      {locationsData &&
+        locationsData?.map((location, idx) => (
+          <div className={styles.location}>
+            <div className={styles.locationHeader}>
+              <span>{idx + 1}</span> {location.region}
+            </div>
+            <ul className={styles.townList}>
+              {location.towns.map((town, i) => (
+                <li key={i}>{town}</li>
+              ))}
+            </ul>
+            <div className={styles.addSection}>
+              <input type="text" />+
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 };
