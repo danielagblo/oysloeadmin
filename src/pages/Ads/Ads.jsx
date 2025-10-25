4e415696d14b8f6317d090094d8800bb96992072
Programming-Sai <saiahprog6@gmail.com>
2025-10-25 08:18:36 +0000

    Working on the Ads page. done with the header.

--- src/pages/Ads/Ads.jsx
+++ src/pages/Ads/Ads.jsx
@@ -1,6 +1,44 @@
 import React from "react";
 import styles from "./ads.module.css";
+import { Caret } from "../../components/SVGIcons/Caret";
+import { SearchIcon } from "../../components/SVGIcons/SearchIcon";
 
 export const Ads = () => {
-  return <div>Ads</div>;
+  return (
+    <div className={styles.adsContainer}>
+      <div className={styles.header}>
+        <div className={styles.dropDownTray}>
+          <button className={styles.dropdown}>
+            <p>Status</p>
+            <button onClick={() => setOpenPromo((p) => !p)}>
+              <Caret />
+            </button>
+          </button>
+          <button className={styles.dropdown}>
+            <p>Promo</p>
+            <button onClick={() => setOpenPromo((p) => !p)}>
+              <Caret />
+            </button>
+          </button>
+          <button className={styles.dropdown}>
+            <p>Ad Type</p>
+            <button onClick={() => setOpenPromo((p) => !p)}>
+              <Caret />
+            </button>
+          </button>
+          <button className={styles.dropdown}>
+            <p>Users</p>
+            <button onClick={() => setOpenPromo((p) => !p)}>
+              <Caret />
+            </button>
+          </button>
+        </div>
+
+        <div className={styles.search}>
+          <SearchIcon />
+          <input type="search" placeholder="Search..." />
+        </div>
+      </div>
+    </div>
+  );
 };
