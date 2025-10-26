ee208610c35cdf9d4527f3f48d2168f9420c35d8
Programming-Sai <saiahprog6@gmail.com>
2025-10-26 18:05:23 +0000

    Done with the first column

--- src/pages/Alerts/Alerts.jsx
+++ src/pages/Alerts/Alerts.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./alerts.module.css";
 import { alertsData } from "../../api/alerts";
 import formatNumber, { timeAgo } from "../../utils/numConverters";
+import { Caret } from "../../components/SVGIcons/Caret";
+import { SendButtonIcon } from "../../components/SVGIcons/SendButtonIcon";
 
 export const Alerts = () => {
   const getAdminById = (id, admins = alertsData?.admins) =>
@@ -13,46 +15,76 @@ export const Alerts = () => {
   return (
     <div className={styles.alertsContainer}>
       <div className={styles.alertBox}>
-        {alertsData?.alerts?.map((alert, idx) => (
-          <div className={styles.alert} key={idx}>
-            <div className={styles.imgName}>
-              <p>By {getAdminById(alert?.senderId)?.name}</p>
+        <div className={styles.alertsHolder}>
+          {alertsData?.alerts?.map((alert, idx) => (
+            <div className={styles.alert} key={idx}>
+              <div className={styles.imgName}>
+                <p>By {getAdminById(alert?.senderId)?.name}</p>
+              </div>
+              <div className={styles.messageBox}>
+                <img
+                  src={getAdminById(alert?.senderId)?.avatar}
+                  className={styles.avatar}
+                />
+                <p>{alert?.message}</p>
+                {alert?.type === "adPromo" ? (
+                  <ul className={styles.adPromo}>
+                    {alert?.relatedAds?.map((ad, idx) => {
+                      const currentAd = getAdById(ad);
+                      return (
+                        <li key={idx} className={styles.adMessage}>
+                          <img src={currentAd?.image} />
+                          <div className={styles.adDetails}>
+                            <h3>{currentAd?.title}</h3>
+                            <p>₵ {formatNumber(currentAd?.price)}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                ) : alert?.type === "coupon" ? (
+                  <div className={styles.coupon}>
+                    <p>
+                      Code: <strong>{alert?.code}</strong>
+                    </p>
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </div>
+              <div className={styles.timeUsers}>
+                <p>~ {alert?.audience} users</p>
+                <p>{timeAgo(alert?.time)}</p>
+              </div>
             </div>
-            <div className={styles.messageBox}>
-              <img
-                src={getAdminById(alert?.senderId)?.avatar}
-                className={styles.avatar}
-              />
-              <p>{alert?.message}</p>
-              {alert?.type === "adPromo" ? (
-                <ul className={styles.adPromo}>
-                  {alert?.relatedAds?.map((ad, idx) => {
-                    const currentAd = getAdById(ad);
-                    return (
-                      <li key={idx} className={styles.adMessage}>
-                        <img src={currentAd?.image} />
-                        <div className={styles.adDetails}>
-                          <h3>{currentAd?.title}</h3>
-                          <p>₵ {formatNumber(currentAd?.price)}</p>
-                        </div>
-                      </li>
-                    );
-                  })}
-                </ul>
-              ) : (
-                <div className={styles.coupon}>
-                  <p>
-                    Code: <strong>{alert?.code}</strong>
-                  </p>
-                </div>
-              )}
-            </div>
-            <div className={styles.timeUsers}>
-              <p>~ {alert?.audience} users</p>
-              <p>{timeAgo(alert?.time)}</p>
+          ))}
+        </div>
+
+        <div className={styles.optionsInputs}>
+          <div className={styles.dropdowns}>
+            <button className={styles.dropdown}>
+              <p>Create Ad</p>
+              <button onClick={() => setOpenPromo((p) => !p)}>
+                <Caret />
+              </button>
+            </button>
+            <button className={styles.dropdown}>
+              <p>Create Coupon</p>
+              <button onClick={() => setOpenPromo((p) => !p)}>
+                <Caret />
+              </button>
+            </button>
+          </div>
+          <div className={styles.chatInputs}>
+            <div className={styles.picInputSend}>
+              <input type="text" placeholder="Type a message..." />
+
+              <button type="button" aria-label="Send message">
+                <SendButtonIcon />
+              </button>
             </div>
           </div>
-        ))}
+        </div>
       </div>
       <div className={styles.usersBox}>Users</div>
     </div>
